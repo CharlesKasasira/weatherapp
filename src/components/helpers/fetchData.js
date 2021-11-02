2edeeb0d9def0e72d486eaefdae4fe79f6b3850a
CharlesKasasira <charleskasasira01@gmail.com>
2021-11-02 15:45:18 +0300

    changed color on bg

--- src/components/helpers/fetchData.js
+++ src/components/helpers/fetchData.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getData = async ( location="Kampala" ) => {
     // const API_URL = `http://localhost:8889/${endpoint}`;
-    const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=93ec627bbff54c968ed133550212210&q=${location}`
+    const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=93ec627bbff54c968ed133550212210&q=${location}`
     let response = '';
 
     try {
